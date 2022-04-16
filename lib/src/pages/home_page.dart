0f022f52a5defcc74d15144d205d5cb5f115643b
ddienu <rockdann@hotmail.com>
2022-04-15 23:15:25 -0500

    Add: Appbar, add: swiper.
    Add the appbar title and cards swiper with 3 items on it.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,9 +9,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hola Mundo'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Películas en cine'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [
+          IconButton(
+            icon: Icon (Icons.search),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            _swiperTarjetas(),
+          ],
+        ),
+      )
+    );
+  }
 
-
+  _swiperTarjetas() {
+    
+    return Container(
+      padding: EdgeInsets.only( top: 10.0 ),
+      width: double.infinity,
+      height: 300.0,
+      child: new Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 300.0,
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          pagination: new SwiperPagination(),
+          control: new SwiperControl(),
+      ),
     );
   }
 }

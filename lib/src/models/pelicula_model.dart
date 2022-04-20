ec42620c125740084719839e09f78c764231ca3e
ddienu <rockdann@hotmail.com>
2022-04-20 00:57:04 -0500

    Update: Pelicula model page
    update the information of the json and return the class to give values to the variables.

--- lib/src/models/pelicula_model.dart
+++ lib/src/models/pelicula_model.dart
@@ -1,23 +1,17 @@
 class Peliculas {
-
   List<Pelicula> items = [];
 
   Peliculas();
 
-  Peliculas.fromJsonList( List<dynamic> jsonList  ) {
+  Peliculas.fromJsonList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
 
-    if ( jsonList == null ) return;
-
-    for ( var item in jsonList  ) {
-      final pelicula = new Pelicula.fromJsonMap(item);
-      items.add( pelicula );
+    for (var item in jsonList) {
+      final pelicula = Pelicula.fromJsonMap(item);
+      items.add(pelicula);
     }
-
   }
-
 }
-
-
 
 class Pelicula {
   int voteCount;
@@ -36,6 +30,7 @@ class Pelicula {
   String releaseDate;
 
   Pelicula({
+
     required this.voteCount,
     required this.id,
     required this.video,
@@ -52,28 +47,24 @@ class Pelicula {
     required this.releaseDate,
   });
 
-  Pelicula.fromJsonMap( Map<String, dynamic> json ) {
-
-    voteCount        = json['vote_count'];
-    id               = json['id'];
-    video            = json['video'];
-    voteAverage      = json['vote_average'] / 1;
-    title            = json['title'];
-    popularity       = json['popularity'] / 1;
-    posterPath       = json['poster_path'];
-    originalLanguage = json['original_language'];
-    originalTitle    = json['original_title'];
-    genreIds         = json['genre_ids'].cast<int>();
-    backdropPath     = json['backdrop_path'];
-    adult            = json['adult'];
-    overview         = json['overview'];
-    releaseDate      = json['release_date'];
-
-  }
-
+  factory Pelicula.fromJsonMap(Map<String, dynamic> json) {
   
 
+    return Pelicula(
 
+    voteCount : json['vote_count'],
+    id : json['id'],
+    video : json['video'],
+    voteAverage : json['vote_average'] / 1,
+    title : json['title'],
+    popularity : json['popularity'] / 1,
+    posterPath : json['poster_path'],
+    originalLanguage : json['original_language'],
+    originalTitle : json['original_title'],
+    genreIds : json['genre_ids'].cast<int>(),
+    backdropPath : json['backdrop_path'],
+    adult : json['adult'],
+    overview : json['overview'],
+    releaseDate : json['release_date'],);
+  }
 }
-
-
